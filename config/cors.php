66b65144bba37e3e3e0bbc69e7abbb294b565b21
John Mak <john.mak@sandboxstrat.com>
2022-04-12 16:33:29 -0400

    fixed cors to process array

--- config/cors.php
+++ config/cors.php
@@ -1,11 +1,13 @@
 <?php 
 
-$allowedOrigins = env('CORS_ALLOWED_ORIGINS');
+$allowedOrigins = explode(",",env('CORS_ALLOWED_ORIGINS'));
+
+Log::info($allowedOrigins);
 
 return [       
     'paths' => ['*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => [$allowedOrigins],
+    'allowed_origins' => $allowedOrigins,
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['Access-Control-Request-Headers','Authorization','content-type','Access-Control-Allow-Origin'],
     'exposed_headers' => [],
