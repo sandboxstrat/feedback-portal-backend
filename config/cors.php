5e889790405c2fd8c7229b6d16478fc1234b4086
John Mak <john.mak@sandboxstrat.com>
2022-04-12 16:37:02 -0400

    Revert "fixed cors to process array"
    This reverts commit 66b65144bba37e3e3e0bbc69e7abbb294b565b21.

--- config/cors.php
+++ config/cors.php
@@ -1,13 +1,11 @@
 <?php 
 
-$allowedOrigins = explode(",",env('CORS_ALLOWED_ORIGINS'));
-
-Log::info($allowedOrigins);
+$allowedOrigins = env('CORS_ALLOWED_ORIGINS');
 
 return [       
     'paths' => ['*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => $allowedOrigins,
+    'allowed_origins' => [$allowedOrigins],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['Access-Control-Request-Headers','Authorization','content-type','Access-Control-Allow-Origin'],
     'exposed_headers' => [],
